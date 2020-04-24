a7012c1641a9a5d3ae39103307eb3b0a20510e65
andrada3 <37013120+andrada3@users.noreply.github.com>
2020-04-24 12:43:57 +0300

    Update seed2.groovy

--- seed2.groovy
+++ seed2.groovy
@@ -6,7 +6,7 @@ def jsonSlurper2 = new JsonSlurper()
 
 //update the filepath with the path of your job.
 
-def jobs = new BufferedReader(new InputStreamReader(new FileInputStream( "/var/lib/jenkins/andrada3/myRepositoryJenkins/seed2.json"),"UTF-8"))
+def jobs = new BufferedReader(new InputStreamReader(new FileInputStream( "/var/lib/jenkins/workspace/andrada3/myRepositoryJenkins/seed2.json"),"UTF-8"))
 data2 = jsonSlurper2.parse(jobs)
 def allSJObs = data2.jobs as Object
 data2.jobs.each
